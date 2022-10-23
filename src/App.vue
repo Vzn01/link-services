10ca94952ec7340840ad1aebc5ce231df18314ab
PEdro S. Vanzan <pv.vanzan@gmail.com>
2022-10-23 16:26:39 -0300

    Upload do projeto

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import item from "./components/item.vue"
+</script>
+
+<script>
+import Dados from "./Dados/services.json"
+  export default{
+    data(){ 
+      return{
+      servicos: null
+      }
+    },
+    mounted(){
+      this.servicos = Dados;
+      //console.log(Dados)
+    }
+    
+  }
+</script>
+
+<template>
+  <img src="./assets/vue.svg" style="width:100px" class="logo vue" alt="Vue logo" />
+  <h1>Seja Bem vindo(a)</h1>
+  <hr/>
+  <div class="itens">
+    <item
+    v-for="servico, index in servicos"
+    :key="index"
+    :item="servico"
+    />
+  </div>
+</template>
+
+<style>
+  .itens{
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  html{
+    background: url("https://images.alphacoders.com/121/1218911.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 110%;
+  }
+</style>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+        <a :href="item.href" target="_blank">
+            <img :src="item.img" alt="Imagem do servico" />
+        </a>
+        <span>{{ item.titulo}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['item']
+}
+</script>
+
+<style scoped>
+    div{
+        margin: 20px 50px 20px 50px;
+        display: block;
+        width: 100px;
+    }
+    img{
+        width: 80px;
+    }
+    span{
+        font-weight: 900;
+    }
+    .card{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        border: 1px solid gray;
+        border-radius: 5%;
+        min-width: 120px;
+        min-height: 150px;
+        box-shadow: 0 0 2em gray;
+        opacity: .9;
+        transition: box-shadow .5;
+       
+    }
+    .card:hover{
+        box-shadow: 0 0 4em gray;
+        opacity: 1;
+
+    }
+
+</style>
